bd3ee937087d9a21445f756ce89057bc01f30c09
Sultan Qasim Khan <sultanqasim@gmail.com>
2015-06-15 11:03:31 -0400

    Enable recovery date/time fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,6 @@ TARGET_BOARD_PLATFORM := msm8916
 TARGET_BOOTLOADER_BOARD_NAME := MSM8916
 TARGET_NO_BOOTLOADER := true
 
-
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M androidboot.bootdevice=soc.0 utags.blkdev=/dev/block/platform/soc.0/by-name/utags utags.backup=/dev/block/platform/soc.0/by-name/utagsBackup movablecore=160M
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_RAMDISK_OFFSET := 0x01000000
@@ -43,7 +42,7 @@ TARGET_KERNEL_SOURCE := kernel/motorola/msm8916
 TARGET_KERNEL_CONFIG := msm8916-mini_defconfig
 
 BOARD_USES_QCOM_HARDWARE := true
-
+TARGET_RECOVERY_QCOM_RTC_FIX := true
 
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
