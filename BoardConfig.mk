49526a230998d1bece7dd43df75adcac12b1ec9a
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-05-03 22:03:51 -0400

    osprey: switch to common TWRP device tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,41 +14,11 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/motorola/osprey
+DEVICE_PATH := device/motorola/osprey
 
-TARGET_ARCH := arm
-TARGET_CPU_ABI  := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a7
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
+include device/motorola/msm8916-common/BoardConfigCommon.mk
 
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-
-TARGET_BOARD_PLATFORM := msm8916
-
-TARGET_BOOTLOADER_BOARD_NAME := MSM8916
-TARGET_NO_BOOTLOADER := true
-
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 vmalloc=400M androidboot.bootdevice=soc.0 movablecore=160M androidboot.selinux=permissive
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_RAMDISK_OFFSET := 0x01000000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
-TARGET_KERNEL_SOURCE := kernel/motorola/msm8916
 TARGET_KERNEL_CONFIG := osprey-mini_defconfig
-
-BOARD_USES_QCOM_HARDWARE := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-TARGET_USES_MOTOROLA_LOG := true
-
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
-TARGET_RECOVERY_FSTAB := device/motorola/osprey/twrp.fstab
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
@@ -57,21 +27,5 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16879616  # 16484 * 1024 mmcblk0p32
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2432696320  # 2375680 * 1024 mmcblk0p41
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 4865261568 # 4751232 * 1024 mmcblk0p42
 
-RECOVERY_SDCARD_ON_DATA := true
-
-# don't take forever to wipe
-BOARD_SUPPRESS_SECURE_ERASE := true
-COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
-
-# Crypto
-TARGET_HW_DISK_ENCRYPTION := true
-TW_INCLUDE_CRYPTO := true
-
 # TWRP
 TW_THEME := portrait_hdpi
-TARGET_RECOVERY_PIXEL_FORMAT := RGB_565
-TW_NEW_ION_HEAP := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TW_SCREEN_BLANK_ON_BOOT := true
-
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
